f1c7c2c3518b9a139e4e111aae6222c4d8fbb212
tirthadas9775 <75427198+tirthadas9775@users.noreply.github.com>
2022-01-04 22:11:47 +0600

    event listeners and projects section added

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 $(window).scroll(function() {
-    if ($(window).scrollTop() > 50) {
+    if ($(window).scrollTop() > 60) {
         $("nav").css({ "background-color": "var(--bg-color)" });
     } else {
         $("nav").css({ "background-color": "" });
     }
 });
+
+$('.fa-cog').click(function() {
+    $('.settings-menu').toggle(300);
+})
